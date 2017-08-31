69df084e3f1811cfd4c8f2d9887838bd77c6fc65
dangminhhieu <dangminhhieu0410@gmail.com>
2017-08-31 10:35:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pc/layouta/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pc.layouta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit_text;
+    TextView text_view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Thiết lập giao diện lấy từ file main.xml
+        setContentView(R.layout.activity_main);
+        //Lấy về các thành phần trong main.xml thông qua id
+        final EditText edit = (EditText)
+                findViewById(R.id.edit_text);
+        final TextView text = (TextView)
+                findViewById(R.id.text_view);
+        //Thiết lập xử lý cho sự kiện nhấn enter
+        edit.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode,
+                                 KeyEvent event) {
+                if (event.getAction() ==
+                        KeyEvent.ACTION_DOWN
+                        && keyCode ==
+                        KeyEvent.KEYCODE_ENTER) {
+                    text.setText(edit.getText().toString());
+                    edit.setText("");
+                    return true;
+                }
+                else {
+                    return false;
+                    }
+            } });
+
+
+    }
+}
